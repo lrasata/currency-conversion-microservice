61aa14dd305147548d872a5b56a7737df1a058e1
lrasata <rmliantsoa@gmail.com>
2022-02-28 18:19:10 +1300

    setting up conversion service getting info from conversion exchange service

---
+++ src/test/java/com/lrasata/microservices/currencyconversionmicroservice/CurrencyConversionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrasata.microservices.currencyconversionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
